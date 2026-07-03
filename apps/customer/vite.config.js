244f8f43a365d8918ad03ff0edba3137f11bfa12
Robera Wakjira <robarobera50@gmail.com>
2026-07-03 09:58:11 +0300

    fix: version conflict with tailwind css

--- apps/customer/vite.config.js
+++ apps/customer/vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()], 
   resolve: {
     alias: {
       '@ethio-buna/shared': path.resolve(
@@ -13,9 +12,8 @@ export default defineConfig({
       ),
     },
   },
-
   server: {
-    port: 3000,
+    port: 3000, 
     fs: {
       allow: ['..', '../../packages/shared', '../../node_modules'],
     },
