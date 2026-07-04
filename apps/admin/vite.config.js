86c05ea763b51e9699695124ff8cb8f9aaa00433
Robera Wakjira <robarobera50@gmail.com>
2026-07-04 10:03:32 +0300

    setup vite config to get shared package

--- apps/admin/vite.config.js
+++ apps/admin/vite.config.js
@@ -1,9 +1,29 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   plugins: [react()],
+  css: {
+    postcss: path.resolve(__dirname, 'postcss.config.js'),
+  },
+  resolve: {
+    alias: {
+      '@ethio-buna/shared': path.resolve(
+        __dirname,
+        '../../packages/shared/index.js',
+      ),
+      '/packages/shared': path.resolve(__dirname, '../../packages/shared'),
+    },
+  },
   server: {
     port: 3001,
+    fs: {
+      allow: ['..', '../../packages/shared', '../../node_modules'],
+    },
   },
+  root: path.resolve(__dirname),
 });
