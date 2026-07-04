8275f5792f0bf4c0d75f53dc00a336d829e97265
Robera Wakjira <robarobera50@gmail.com>
2026-07-04 09:55:18 +0300

    remove the path from vite config to get from node modules

--- apps/admin/vite.config.js
+++ apps/admin/vite.config.js
@@ -1,18 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@ethio-buna/shared': path.resolve(__dirname, '../../packages/shared'),
-    },
-  },
   server: {
     port: 3001,
-    fs: {
-      allow: ['..', '../../packages/shared', '../../node_modules'],
-    },
   },
 });
